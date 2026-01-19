70d1cda21aec1e9f43ee7c43b04d2fbfa34004c6
Hunter Phillips <hkphillips42@gmail.com>
2026-01-19 11:28:33 -0600

    multi-provider support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
-import { mockApiPlugin, snImportPlugin } from './vite-plugins'
+import { mockApiPlugin, snImportPlugin, aiGeneratePlugin } from './vite-plugins'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mockApiPlugin(), snImportPlugin()],
+  plugins: [react(), mockApiPlugin(), snImportPlugin(), aiGeneratePlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
